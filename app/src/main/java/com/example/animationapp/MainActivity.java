966063bc84e3a2b455fcc7071f2c8a607b56ca2f
benedict.b.contawe <benedict.b.contawe@accenture.com>
2019-07-12 19:16:46 +0800

    Update thumb animation

--- app/src/main/java/com/example/animationapp/MainActivity.java
+++ app/src/main/java/com/example/animationapp/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.animationapp;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private AnimationDrawable animationDrawable;
+    private Button button;
     private ImageView imageView;
-    private AppCompatButton button;
-    private float currentDegrees;
-    private boolean isExpanded;
+    //private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,112 +24,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView) findViewById(R.id.imageView);
-        button = (AppCompatButton) findViewById(R.id.button);
-        button.setOnClickListener(this);
+        imageView.setBackgroundResource(R.drawable.custom_animation);
 
-        currentDegrees = 0;
-        isExpanded = false;
+        animationDrawable = (AnimationDrawable) imageView.getBackground();
+
+        button =(Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        //boolean x = isExpanded ? true : false;
-
-        if (isExpanded){
-            turnArrowCounterClockwiseUp(imageView);
-        }
-        else {
-            turnArrowClockwiseDown(imageView);
-
-        }
-    }
-
-    @Deprecated
-    private void turnArrowClockwiseDown_Deprecated(){
-        //RotateAnimation anim = new RotateAnimation(currentRotation, currentRotation + 30, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,0.5f);
-        //long currentRotation; = (currentRotation + 30) % 360;
-
-        RotateAnimation anim = new RotateAnimation(currentDegrees, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,0.5f);
-        anim.setInterpolator(new LinearInterpolator());
-        anim.setDuration(500);
-        anim.setFillEnabled(true);
-        anim.setFillAfter(true);
-        anim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                Toast.makeText(getApplication(),"onAnimationEnd turnArrowDownA()",Toast.LENGTH_SHORT).show();
-                currentDegrees = 180;
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-
-
-        imageView.startAnimation(anim);
-    }
-
-    @Deprecated
-    private void turnArrowCounterClockwiseUp_Deprecated(){
-        RotateAnimation anim = new RotateAnimation(currentDegrees, 0, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,0.5f);
-        anim.setInterpolator(new LinearInterpolator());
-        anim.setDuration(500);
-        anim.setFillEnabled(true);
-        anim.setFillAfter(true);
-        anim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                Toast.makeText(getApplication(),"onAnimationEnd turnArrowUpA()",Toast.LENGTH_SHORT).show();
-                currentDegrees = 0;
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-
-
-        imageView.startAnimation(anim);
-    }
-
-    private void turnArrowClockwiseDown(ImageView imageView){
-        ObjectAnimator imageViewObjectAnimator = ObjectAnimator.ofFloat(imageView , View.ROTATION, currentDegrees, 180f);
-        imageViewObjectAnimator.setDuration(500);
-        imageViewObjectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                Log.e(MainActivity.class.getSimpleName(),valueAnimator.getAnimatedValue().toString());
-                currentDegrees = (float) valueAnimator.getAnimatedValue();
-            }
-        });
-        imageViewObjectAnimator.start();
-        isExpanded = true;
-    }
-
-    private void turnArrowCounterClockwiseUp(ImageView imageView){
-        ObjectAnimator imageViewObjectAnimator = ObjectAnimator.ofFloat(imageView , View.ROTATION, currentDegrees, 0f);
-        imageViewObjectAnimator.setDuration(500);
-        imageViewObjectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                Log.e(MainActivity.class.getSimpleName(),valueAnimator.getAnimatedValue().toString());
-                currentDegrees = (float) valueAnimator.getAnimatedValue();
-            }
-        });
-        imageViewObjectAnimator.start();
-        isExpanded = false;
+        animationDrawable.stop();
+        //animationDrawable.selectDrawable(0);
+        animationDrawable.start();
     }
 }
