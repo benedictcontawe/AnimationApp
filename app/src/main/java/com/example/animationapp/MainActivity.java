c4b75b971f18a49c0e9103c15f83ba48ab0601d4
Benedict <benedict.b.contawe@accenture.com>
2019-10-04 10:59:47 +0800

    Branch out seek bar thumb animation

--- app/src/main/java/com/example/animationapp/MainActivity.java
+++ app/src/main/java/com/example/animationapp/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.animationapp;
 
 import android.graphics.drawable.AnimationDrawable;
-import android.nfc.Tag;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SeekBar;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -19,8 +13,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private AnimationDrawable animationDrawable;
     private Button button;
     private ImageView imageView;
-    private SeekBar seekBar;
-    private int selectedThumb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +20,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView) findViewById(R.id.imageView);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
         button =(Button) findViewById(R.id.button);
-
-        //seekBar.setPadding(0,0,0,0);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //Log.e("MainActivity ","Seek Bar Tag " + seekBar.getTag().toString());
-        //Log.e("MainActivity ","Seek Bar Thumb " + seekBar.getThumb().toString());
-        if (seekBar.getTag() == null){
-            Log.e(MainActivity.class.getSimpleName(),"seeker tag null");
-        }
-        else {
-            Log.e(MainActivity.class.getSimpleName(),"seeker tag not null");
-        }
-
-        selectedThumb = -7;
 
         imageView.setBackgroundResource(R.drawable.custom_animation);
         animationDrawable = (AnimationDrawable) imageView.getBackground();
@@ -65,98 +44,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         animationDrawable.stop();
         //animationDrawable.selectDrawable(0);
         animationDrawable.start();
-
-        new CountDownTimer(1000, 40){
-            public void onTick(long millisUntilFinished){
-
-                switch (selectedThumb){
-                    case 1:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_1st));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_1st);
-                        selectedThumb = 2;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_1st");
-                        break;
-                    case 2:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_2nd));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_2nd);
-                        selectedThumb = 3;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_2nd");
-                        break;
-                    case 3:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_3rd));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_3rd);
-                        selectedThumb = 4;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_3rd");
-                        break;
-                    case 4:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_4th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_4th);
-                        selectedThumb = 5;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_4th");
-                        break;
-                    case 5:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_5th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_5th);
-                        selectedThumb = 6;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_5th");
-                        break;
-                    case 6:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_6th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_6th);
-                        selectedThumb = 7;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_6th");
-                        break;
-                    case 7:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_7th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_7th);
-                        selectedThumb = 8;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_7th");
-                        break;
-                    case 8:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_7th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_7th);
-                        selectedThumb = 9;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_7th");
-                        break;
-                    case 9:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_7th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_7th);
-                        selectedThumb = 10;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_7th");
-                        break;
-                    case 10:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_6th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_6th);
-                        selectedThumb = 11;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_6th");
-                        break;
-                    case 11:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_5th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_5th);
-                        selectedThumb = 11;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_5th");
-                        break;
-                    default:
-                        seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_0th));
-                        seekBar.setTag(R.drawable.ic_seeker_thumb_selected_0th);
-                        selectedThumb++;
-                        Log.e(MainActivity.class.getSimpleName(),"ic_seeker_thumb_selected_0th");
-                        break;
-                }
-            }
-            public  void onFinish(){
-                //seekBar.setThumb(ContextCompat.getDrawable(this,R.drawable.ic_seeker_thumb_selected_5th))
-                seekBar.setThumb(getResources().getDrawable(R.drawable.ic_seeker_thumb_selected_5th));
-                seekBar.setTag(R.drawable.ic_seeker_thumb_selected_5th);
-                selectedThumb = -7;
-                Log.e(MainActivity.class.getSimpleName(),"onFinish()");
-                if (seekBar.getTag().equals(R.drawable.ic_seeker_thumb_selected_5th)){
-                    Log.e(MainActivity.class.getSimpleName(),"seeker thumb equal");
-                }
-                else{
-                    Log.e(MainActivity.class.getSimpleName(),"seeker thumb not equal");
-                }
-            }
-        }.start();
     }
 }
