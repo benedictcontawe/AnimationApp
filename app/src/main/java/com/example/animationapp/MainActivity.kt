fab8d261afcf00d1e9f0cd240186a5dba50c782f
Benedict Contawe <benedictcontawe@gmail.com>
2022-12-02 03:23:35 +0800

    New Branch for Animated Vector Drawable

--- app/src/main/java/com/example/animationapp/MainActivity.kt
+++ app/src/main/java/com/example/animationapp/MainActivity.kt
@@ -2,12 +2,53 @@ package com.example.animationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.databinding.DataBindingUtil
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import com.example.animationapp.databinding.MainBinder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private var binder : MainBinder? = null
+    private var isPlay : Boolean? = null
+
     override fun onCreate(savedInstanceState : Bundle?) {
-        installSplashScreen()
+        binder = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        binder?.setLifecycleOwner(this@MainActivity)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initializeAnimation()
+    }
+
+    private fun initializeAnimation() {
+        binder?.floatingActionButton?.setImageDrawable(
+            getAnimatedVectorDrawable()
+        )
+        binder?.floatingActionButton?.setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(view : View?) {
+        if (view == binder?.floatingActionButton) {
+            toggleBoolean()
+            binder?.floatingActionButton?.setImageDrawable(
+                getAnimatedVectorDrawable()
+            )
+            val animatable : Animatable2Compat = binder?.floatingActionButton?.getDrawable() as Animatable2Compat
+            animatable.start()
+        }
+    }
+
+    private fun toggleBoolean() {
+        if (isPlay == true)
+            isPlay = false
+        else
+            isPlay = true
+    }
+
+    private fun getAnimatedVectorDrawable() : AnimatedVectorDrawableCompat? {
+        val play : AnimatedVectorDrawableCompat? = AnimatedVectorDrawableCompat.create(this@MainActivity, R.drawable.anim_play_to_pause)
+        val pause : AnimatedVectorDrawableCompat? = AnimatedVectorDrawableCompat.create(this@MainActivity, R.drawable.anim_pause_to_play)
+        return if (isPlay == true) pause else play
     }
 }
